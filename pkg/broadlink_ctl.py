ceeb096703d4e687b341cfb0dba9645042806b6c
mao2009 <en>
2018-08-24 09:02:09 +0900

    mod

--- pkg/broadlink_ctl.py
+++ pkg/broadlink_ctl.py
@@ -87,7 +87,7 @@ def show_device_info(device, device_number):
 def ask_overwrite(path):
     if os.path.exists(path):
         while True:
-            response = input('File already exists,do you overwrite?(y/n):')
+            response = input(r'File already exists,do you overwrite?(y/n):')
             response = response.lower()
             if response == 'y' or response == 'yes':
                 return True
